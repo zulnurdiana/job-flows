59c15896960ced8e4a64d71a9f3af291db026007
Zulfikar Nurdiana <66067543+zulnurdiana@users.noreply.github.com>
2024-05-12 20:34:07 +0700

    time: 3:47:00

--- src/app/job/new/NewJobForm.tsx
+++ src/app/job/new/NewJobForm.tsx
@@ -1,8 +1,42 @@
 "use client";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import H1 from "@/components/ui/h1";
+import { createJobsSchema, createJobValue } from "@/lib/validation";
 import React from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/ui/input";
+import Select from "@/components/ui/select";
+import { jobTypes, locationTypes } from "@/lib/job-types";
+import LocationInput from "@/components/LocationInput";
+import { X } from "lucide-react";
 
 const NewJobForm = () => {
+  const form = useForm<createJobValue>({
+    resolver: zodResolver(createJobsSchema),
+  });
+
+  const {
+    handleSubmit,
+    watch,
+    trigger,
+    control,
+    setValue,
+    setFocus,
+    formState: { isSubmitting },
+  } = form;
+
+  async function onSubmit(value: createJobValue) {
+    alert(JSON.stringify(value, null, 2));
+  }
+
   return (
     <main className="max-w-[350px] sm:max-w-3xl m-auto space-y-10 my-10">
       <div className="space-y-5 text-center">
@@ -16,6 +50,135 @@ const NewJobForm = () => {
         <p className="text-muted-foreground">
           Provide job description and detail
         </p>
+        <Form {...form}>
+          <form onSubmit={handleSubmit(onSubmit)} noValidate>
+            <FormField
+              control={control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Job Title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="e.g Frontend Developer" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={control}
+              name="type"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Job Type</FormLabel>
+                  <FormControl>
+                    <Select>
+                      <option value="" hidden>
+                        Select Job Type
+                      </option>
+                      {jobTypes.map((jobType) => (
+                        <option value={jobType} key={jobType}>
+                          {jobType}
+                        </option>
+                      ))}
+                    </Select>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={control}
+              name="companyName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Company</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={control}
+              name="companyLogo"
+              render={({ field: { value, ...fieldValues } }) => (
+                <FormItem>
+                  <FormLabel>Company</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...fieldValues}
+                      type="file"
+                      accept="image/*"
+                      // handle jika user menginput lebih dari 1 image
+                      onChange={(e) => {
+                        const file = e.target.files?.[0];
+                        fieldValues.onChange(file);
+                      }}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={control}
+              name="locationType"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Location</FormLabel>
+                  <FormControl>
+                    <Select>
+                      <option value="" hidden>
+                        Select an option
+                      </option>
+                      {locationTypes.map((locationType) => (
+                        <option value={locationType} key={locationType}>
+                          {locationType}
+                        </option>
+                      ))}
+                    </Select>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={control}
+              name="location"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Office Location</FormLabel>
+                  <FormControl>
+                    <LocationInput
+                      onLocationSelected={field.onChange}
+                      ref={field.ref}
+                    />
+                  </FormControl>
+                  {watch("location") && (
+                    <button
+                      type="button"
+                      onClick={() =>
+                        setValue("location", "", { shouldValidate: true })
+                      }
+                      className="flex items-center gap-2"
+                    >
+                      <X size={20} />
+                      <span>{watch("location")}</span>
+                    </button>
+                  )}
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
       </div>
     </main>
   );
---
+++ src/components/LocationInput.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { forwardRef, useMemo, useState } from "react";
+import { Input } from "./ui/input";
+import citiesList from "@/lib/cities-list";
+
+interface LocationInputProps
+  extends React.HtmlHTMLAttributes<HTMLInputElement> {
+  onLocationSelected: (location: string) => void;
+}
+
+// Ref digunakan untuk klick label, fokus langsung ke input dll
+
+export default forwardRef<HTMLInputElement, LocationInputProps>(
+  function LocationInput({ onLocationSelected, ...props }, ref) {
+    const [locationSearchInput, setLocationSearchInput] = useState("");
+    const [hasFocus, setHasFocus] = useState(false);
+
+    const cities = useMemo(() => {
+      if (!locationSearchInput.trim()) return [];
+
+      const searchWords = locationSearchInput.split(" ");
+
+      return citiesList
+        .map((city) => `${city.name}, ${city.subcountry}, ${city.country}`)
+        .filter(
+          (city) =>
+            city.toLowerCase().startsWith(searchWords[0].toLowerCase()) &&
+            searchWords.every((word) =>
+              city.toLowerCase().includes(word.toLowerCase()),
+            ),
+        )
+        .slice(0, 5);
+    }, [locationSearchInput]);
+
+    return (
+      <div className="relative">
+        <Input
+          placeholder="Seach for a city"
+          type="search"
+          value={locationSearchInput}
+          onChange={(e) => setLocationSearchInput(e.target.value)}
+          onFocus={() => setHasFocus(true)}
+          onBlur={() => setHasFocus(false)}
+          {...props}
+          ref={ref}
+        />
+        {locationSearchInput.trim() && hasFocus && (
+          <div className="absolute w-full z-20 shadow-lg divide-y bg-background border-x-2 border-b">
+            {!cities.length && <p className="p-3">No cities found</p>}
+            {cities.map((city) => (
+              <button
+                key={city}
+                className="block w-full text-start p-2"
+                onMouseDown={(e) => {
+                  e.preventDefault();
+                  onLocationSelected(city);
+                  setLocationSearchInput("");
+                }}
+              >
+                {city}
+              </button>
+            ))}
+          </div>
+        )}
+      </div>
+    );
+  },
+);
